3e015000a8113bed8ef7a0ca2e9381d0cc139df7
Mauro Amico <mauro.amico@unibo.it>
2021-10-14 12:20:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='pas.plugins.oidc',
-    version='1.0a1',
+    version='1.0a2',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
