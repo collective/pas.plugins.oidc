efc6ee69be35bfe0f97a207b45a6d6e74eed80c8
Mauro Amico <mauro.amico@gmail.com>
2023-04-05 00:05:07 +0200

    Back to development: 1.0a6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="pas.plugins.oidc",
-    version='1.0a5',
+    version='1.0a6.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
