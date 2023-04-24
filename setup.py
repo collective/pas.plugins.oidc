f7c9d9319eba7bd8e7fbbe71bb55f96d23aff15b
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-04-24 14:51:22 +0200

    remove python2

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         "Framework :: Plone :: Addon",
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
