d8182359318a1843ea4007052b8a1c6a6aa8b48b
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-04-24 14:50:41 +0200

    add missing

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7"
+    python_requires=">=3.7",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
