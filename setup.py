68e1e0c614f85b0912e35b6510063fae8fb092e9
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-04-24 14:46:37 +0200

    declare support only for python >= 3.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="pas.plugins.oidc",
-    version='1.0a6.dev0',
+    version="1.0a6.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
@@ -51,7 +51,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+    python_requires=">=3.7"
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
