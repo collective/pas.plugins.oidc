4ba6f0b3222408dab16c893e8db2a76108428b1e
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-09-13 22:49:44 +0200

    Back to development: 1.0b4+cs.8
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="pas.plugins.oidc",
-    version="1.0b4+cs.7",
+    version="1.0b4+cs.8.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     long_description_content_type="text/x-rst",
