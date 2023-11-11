139b5a73963b9b870acb714fa4a0e954aacf17fa
Mauro Amico <mauro.amico@gmail.com>
2023-11-11 02:42:50 +0100

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="pas.plugins.oidc",
-    version="1.0.0",
+    version="1.0.1.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     long_description_content_type="text/x-rst",
