ccf7edf1982e6f06791832cb44b63b833b15eca8
Érico Andrei <ericof@gmail.com>
2024-06-25 20:54:11 -0300

    Back to development: 2.0.0b2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = f"""
 
 setup(
     name="pas.plugins.oidc",
-    version="2.0.0b1",
+    version="2.0.0b2.dev0",
     description="OIDC support for Plone sites",
     long_description=long_description,
     long_description_content_type="text/markdown",
