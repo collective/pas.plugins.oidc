861824661d669e467d5a23b986c6695ae22ded00
Rémi Dubois <remi.dubois@gmail.com>
2025-06-28 21:16:59 +0200

    feat: allow to specify userinfo endpoint method

--- src/pas/plugins/oidc/browser/view.py
+++ src/pas/plugins/oidc/browser/view.py
@@ -118,6 +118,7 @@ class CallbackView(BrowserView):
         args, state = utils.parse_authorization_response(
             self.context, qs, client, session
         )
+        method = self.context.getProperty("userinfo_endpoint_method", "POST")
         if self.context.getProperty("use_modified_openid_schema"):
             IdToken.c_param.update({
                 "email_verified": utils.SINGLE_OPTIONAL_BOOLEAN_AS_STRING,
@@ -126,7 +127,7 @@ class CallbackView(BrowserView):
 
         # The response you get back is an instance of an AccessTokenResponse
         # or again possibly an ErrorResponse instance.
-        user_info = utils.get_user_info(client, state, args)
+        user_info = utils.get_user_info(client, state, args, method)
         if user_info:
             self.context.rememberIdentity(user_info)
             self.request.response.setHeader(
